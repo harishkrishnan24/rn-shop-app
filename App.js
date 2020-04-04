949ee01d2609966f470f972c3b2d80a237237560
Harish Krishnan <harishkrishnan1993@gmail.com>
2020-04-04 19:36:11 +0530

    Add cart reducer and actions

--- App.js
+++ App.js
@@ -2,12 +2,15 @@ import React, { useState } from 'react';
 import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 import productsReducer from './store/reducers/products';
+import cartReducer from './store/reducers/cart';
 import ShopNavigator from './navigation/ShopNavigator';
 import { AppLoading } from 'expo';
 import * as Font from 'expo-font';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const rootReducer = combineReducers({
-  products: productsReducer
+  products: productsReducer,
+  cart: cartReducer
 });
 
 const store = createStore(rootReducer);
