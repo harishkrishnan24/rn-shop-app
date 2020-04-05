789362483dd39748e5e340bed3c79a83c8f7c495
Harish Krishnan <harishkrishnan1993@gmail.com>
2020-04-05 12:09:47 +0530

    Add Orders screen implementation

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -1,28 +1,76 @@
-import { createStackNavigator } from 'react-navigation-stack';
-import ProductsOverviewScreen from '../screens/shop/ProductsOverviewScreen';
-import { Platform } from 'react-native';
-import Colors from '../constants/Colors';
-import { createAppContainer } from 'react-navigation';
-import ProductDetailScreen from '../screens/shop/ProductDetailScreen';
-import CartScreen from '../screens/shop/CartScreen';
+import React from "react";
+import { createStackNavigator } from "react-navigation-stack";
+import { createDrawerNavigator } from "react-navigation-drawer";
+import ProductsOverviewScreen from "../screens/shop/ProductsOverviewScreen";
+import { Platform } from "react-native";
+import Colors from "../constants/Colors";
+import { createAppContainer } from "react-navigation";
+import ProductDetailScreen from "../screens/shop/ProductDetailScreen";
+import CartScreen from "../screens/shop/CartScreen";
+import OrdersScreen from "../screens/shop/OrdersScreen";
+import { Ionicons } from "@expo/vector-icons";
 
-const ProductsNavigator = createStackNavigator({
-    ProductsOverview: ProductsOverviewScreen,
-    ProductDetail: ProductDetailScreen,
-    Cart: CartScreen
-}, {
-    defaultNavigationOptions: {
-        headerStyle: {
-            backgroundColor: Platform.OS === 'android' ? Colors.primary : '',
-        },
-        headerTitleStyle: {
-            fontFamily: 'open-sans-bold',
-        },
-        headerBackTitleStyle: {
-            fontFamily: 'open-sans',
-        },
-        headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary
-    }
-});
+const defaultNavOptions = {
+	headerStyle: {
+		backgroundColor: Platform.OS === "android" ? Colors.primary : "",
+	},
+	headerTitleStyle: {
+		fontFamily: "open-sans-bold",
+	},
+	headerBackTitleStyle: {
+		fontFamily: "open-sans",
+	},
+	headerTintColor: Platform.OS === "android" ? "white" : Colors.primary,
+};
 
-export default createAppContainer(ProductsNavigator);
+const ProductsNavigator = createStackNavigator(
+	{
+		ProductsOverview: ProductsOverviewScreen,
+		ProductDetail: ProductDetailScreen,
+		Cart: CartScreen,
+	},
+	{
+		defaultNavigationOptions: defaultNavOptions,
+		navigationOptions: {
+			drawerIcon: (drawerConfig) => (
+				<Ionicons
+					name={Platform.OS === "android" ? "md-cart" : "ios-cart"}
+					size={23}
+					color={drawerConfig.tintColor}
+				/>
+			),
+		},
+	}
+);
+
+const OrdersNavigator = createStackNavigator(
+	{
+		Orders: OrdersScreen,
+	},
+	{
+		defaultNavigationOptions: defaultNavOptions,
+		navigationOptions: {
+			drawerIcon: (drawerConfig) => (
+				<Ionicons
+					name={Platform.OS === "android" ? "md-list" : "ios-list"}
+					size={23}
+					color={drawerConfig.tintColor}
+				/>
+			),
+		},
+	}
+);
+
+const ShopNavigator = createDrawerNavigator(
+	{
+		Products: ProductsNavigator,
+		Orders: OrdersNavigator,
+	},
+	{
+		contentOptions: {
+			activeTintColor: Colors.primary,
+		},
+	}
+);
+
+export default createAppContainer(ShopNavigator);
--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,45 +1,65 @@
-import React from 'react';
-import { FlatList, Platform } from 'react-native';
-import { useSelector, useDispatch } from 'react-redux'
-import ProductItem from '../../components/shop/ProductItem';
-import * as cartActions from '../../store/actions/cart';
-import { HeaderButtons, Item } from 'react-navigation-header-buttons';
-import HeaderButton from '../../components/UI/HeaderButton';
+import React from "react";
+import { FlatList, Platform } from "react-native";
+import { useSelector, useDispatch } from "react-redux";
+import ProductItem from "../../components/shop/ProductItem";
+import * as cartActions from "../../store/actions/cart";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import HeaderButton from "../../components/UI/HeaderButton";
 
-const ProductsOverviewScreen = props => {
-    const products = useSelector(state => state.products.availableProducts);
-    const dispatch = useDispatch();
+const ProductsOverviewScreen = (props) => {
+	const products = useSelector((state) => state.products.availableProducts);
+	const dispatch = useDispatch();
 
-    return (<FlatList
-        data={products}
-        keyExtractor={item => item.id}
-        renderItem={itemData => <ProductItem
-            image={itemData.item.imageUrl}
-            title={itemData.item.title}
-            price={itemData.item.price}
-            onViewDetail={() => {
-                props.navigation.navigate('ProductDetail', { productId: itemData.item.id, productTitle: itemData.item.title });
-            }}
-            onAddToCart={() => {
-                dispatch(cartActions.addToCart(itemData.item));
-            }}
-        />}
-    />);
+	return (
+		<FlatList
+			data={products}
+			keyExtractor={(item) => item.id}
+			renderItem={(itemData) => (
+				<ProductItem
+					image={itemData.item.imageUrl}
+					title={itemData.item.title}
+					price={itemData.item.price}
+					onViewDetail={() => {
+						props.navigation.navigate("ProductDetail", {
+							productId: itemData.item.id,
+							productTitle: itemData.item.title,
+						});
+					}}
+					onAddToCart={() => {
+						dispatch(cartActions.addToCart(itemData.item));
+					}}
+				/>
+			)}
+		/>
+	);
 };
 
-ProductsOverviewScreen.navigationOptions = navData => {
-    return {
-        headerTitle: 'Products Overview',
-        headerRight: () => <HeaderButtons HeaderButtonComponent={HeaderButton}>
-            <Item
-                title="Cart"
-                iconName={Platform.OS === 'android' ? 'md-cart' : 'ios-cart'}
-                onPress={() => {
-                    navData.navigation.navigate('Cart');
-                }}
-            />
-        </HeaderButtons>
-    };
+ProductsOverviewScreen.navigationOptions = (navData) => {
+	return {
+		headerTitle: "Products Overview",
+		headerRight: () => (
+			<HeaderButtons HeaderButtonComponent={HeaderButton}>
+				<Item
+					title='Cart'
+					iconName={Platform.OS === "android" ? "md-cart" : "ios-cart"}
+					onPress={() => {
+						navData.navigation.navigate("Cart");
+					}}
+				/>
+			</HeaderButtons>
+		),
+		headerLeft: () => (
+			<HeaderButtons HeaderButtonComponent={HeaderButton}>
+				<Item
+					title='Cart'
+					iconName={Platform.OS === "android" ? "md-menu" : "ios-menu"}
+					onPress={() => {
+						navData.navigation.toggleDrawer();
+					}}
+				/>
+			</HeaderButtons>
+		),
+	};
 };
 
 export default ProductsOverviewScreen;
